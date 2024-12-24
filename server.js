af3f10099cc46e3f3e77fb08f0ee886326c9c5ef
akashKumarPvt <akash@privatecourt.in>
2024-12-24 15:45:10 +0530

    ec2 testing

--- server.js
+++ server.js
@@ -889,8 +889,7 @@ app.post("/NewWebhookEmail", async(req, res)=>{
   }
 });
 
-// const port = process.env.PORT | 5000;
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-module.exports.handler = serverless(app);
+const port = process.env.PORT | 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
